dfb3009292a548cd43a9e7e876c6e137f3927ac9
Ricardo Gómez <00108517@uca.edu.sv>
2024-01-10 20:52:14 -0600

    added: jquery ajax to send dhis2 data from forms

--- scripts/main.js
+++ scripts/main.js
@@ -29,7 +29,7 @@ $(document).ready(function () {
   function handleIconClick() {
     var selectedValue = $(this).data("value");
     var step3QuizContainer = $(this).closest(".question");
-    console.log("selectedValue: " + selectedValue);
+    //console.log("selectedValue: " + selectedValue);
 
     step3QuizContainer.find(".optns").hide();
     step3QuizContainer
@@ -38,12 +38,13 @@ $(document).ready(function () {
       .show()
       .data("value", selectedValue)
       .off("click")
+      .addClass("final-" + $(this).prop("id"))
       .click(function () {
         // When 'answer-selected' is clicked, hide the "Siguiente" button
         disableAllOtherQuestions($(this));
         $(this).hide().empty();
         step3QuizContainer.find(".optns").show();
-        
+
         $btnNext.hide(); // Hide the "Siguiente" button
         $btnSend.hide();
       });
@@ -53,8 +54,9 @@ $(document).ready(function () {
   function updateNextButtonState() {
     var isStep2an1Checked =
       $('[name="step2an1"]').find('input[type="checkbox"]:checked').length > 0;
-    var isStep2an2Checked =
+    /* var isStep2an2Checked =
       $('[name="step2an2"]').find('input[type="checkbox"]:checked').length > 0;
+      console.log(isStep2an2Checked); */
     var areOptionsVisible = currentStep === 3 && $(".optns:visible").length > 0;
 
     if (currentStep === 1) {
@@ -64,10 +66,12 @@ $(document).ready(function () {
       $btnBack.hide();
 
       $btnSend.hide();
-      if ($('input[type="radio"][name="step2brk1y"]:checked').val() === "Si") {
-        $btnNext.toggle(isStep2an1Checked && isStep2an2Checked);
+      if (
+        $('input[type="radio"][name="step2brk1y"]:checked').attr("id") === "Si"
+      ) {
+        $btnNext.toggle(isStep2an1Checked /* && isStep2an2Checked */);
       } else if (
-        $('input[type="radio"][name="step2brk1y"]:checked').val() === "No"
+        $('input[type="radio"][name="step2brk1y"]:checked').attr("id") === "No"
       ) {
         $btnNext.toggle(isStep2an1Checked);
       } else {
@@ -140,44 +144,221 @@ $(document).ready(function () {
     }
   }
 
-  function disableAllOtherQuestions(section){
-      console.log(section.find('.opt').attr("id"));
-    
-      const questions = [question1,question2,question3,question4,question5,question6,question7];
-      const clickedQuestion = questions.find(question => section.find('.opt').attr("id") == question.find('.opt').attr("id"));
-      const remainingQuestions = questions.filter(question => section.find('.opt').attr("id") != question.find('.opt').attr("id"));
-      clickedQuestion.find('.answer-selected').children == 0;
-      console.log(clickedQuestion.find('.answer-selected').children().length == 1);
-      
-       if(clickedQuestion.find('.answer-selected').children().length == 1){
-        for(let question of remainingQuestions){
-          question.addClass('disabledbutton');
-        }
-      }
-  }
+  function disableAllOtherQuestions(section) {
+    //console.log(section.find(".opt").attr("id"));
 
-  function enableSectionsWithSelectedOptions(){
-    const clickedElementId = $(this).attr("id");
-    console.log(clickedElementId);
-    const questions = [question1,question2,question3,question4,question5,question6,question7];
-    const clickedQuestion = questions.find(question => clickedElementId == question.find('.opt').attr("id"));
-    
-    const remainingQuestions = questions.filter(question => clickedElementId != question.find('.opt').attr("id"));
-    for(let question of remainingQuestions){
-      if(question.find('.answer-selected').children().length == 1 || question.hasClass('currentSelection'))
-      question.removeClass('disabledbutton');
+    const questions = [
+      question1,
+      question2,
+      question3,
+      question4,
+      question5,
+      question6,
+      question7,
+    ];
+    const clickedQuestion = questions.find(
+      (question) =>
+        section.find(".opt").attr("id") == question.find(".opt").attr("id")
+    );
+    const remainingQuestions = questions.filter(
+      (question) =>
+        section.find(".opt").attr("id") != question.find(".opt").attr("id")
+    );
+    clickedQuestion.find(".answer-selected").children == 0;
+    //console.log(clickedQuestion.find(".answer-selected").children().length == 1);
+
+    if (clickedQuestion.find(".answer-selected").children().length == 1) {
+      for (let question of remainingQuestions) {
+        question.addClass("disabledbutton");
+      }
     }
   }
+
+  function enableSectionsWithSelectedOptions() {
+    const clickedElementId = $(this).attr("id");
+    //console.log(clickedElementId);
+    const questions = [
+      question1,
+      question2,
+      question3,
+      question4,
+      question5,
+      question6,
+      question7,
+    ];
+    const clickedQuestion = questions.find(
+      (question) => clickedElementId == question.find(".opt").attr("id")
+    );
+
+    const remainingQuestions = questions.filter(
+      (question) => clickedElementId != question.find(".opt").attr("id")
+    );
+    for (let question of remainingQuestions) {
+      if (
+        question.find(".answer-selected").children().length == 1 ||
+        question.hasClass("currentSelection")
+      )
+        question.removeClass("disabledbutton");
+    }
+  }
+
+  function getUrlQueryParams() {
+    var vars = [],
+      hash;
+    var hashes = window.location.href
+      .slice(window.location.href.indexOf("?") + 1)
+      .split("&");
+    for (var i = 0; i < hashes.length; i++) {
+      hash = hashes[i].split("=");
+      vars.push(hash[0]);
+      vars[hash[0]] = hash[1];
+    }
+    return vars;
+  }
+
+  function sendSurveyData() {
+    let checkboxConsultaGeneral = $("#checkbox-consulta-general").is(
+      ":checked"
+    );
+    let checkboxCitologia = $("#checkbox-citologia").is(":checked");
+    let checkboxConsejeria = $("#checkbox-consejeria").is(":checked");
+    let checkboxEntrega = $("#checkbox-entrega").is(":checked");
+    let checkboxAtencion = $("#checkbox-atencion").is(":checked");
+    let checkboxOtros = $("#checkbox-atencion").is(":checked");
+
+    let wasInformationDeliveredRadioButton = $(
+      "input[name = 'step2brk1y']:checked"
+    ).val();
+    let question1Answer = $(".final-answer1").find(".opt").attr("data-value");
+    let question2Answer = $(".final-answer2").find(".opt").attr("data-value");
+    let question3Answer = $(".final-answer3").find(".opt").attr("data-value");
+    let question4Answer = $(".final-answer4").find(".opt").attr("data-value");
+    let question5Answer = $(".final-answer5").find(".opt").attr("data-value");
+    let question6Answer = $(".final-answer6").find(".opt").attr("data-value");
+    let question7Answer = $(".final-answer7").find(".opt").attr("data-value");
+
+   /*  console.log(checkboxConsultaGeneral);
+    console.log(checkboxCitologia);
+    console.log(checkboxConsejeria);
+    console.log(checkboxEntrega);
+    console.log(checkboxAtencion);
+    console.log(checkboxOtros);
+    console.log(wasInformationDeliveredRadioButton);
+    console.log(question1Answer);
+    console.log(question2Answer);
+    console.log(question3Answer);
+    console.log(question4Answer);
+    console.log(question5Answer);
+    console.log(question6Answer);
+    console.log(question7Answer); */
+    const url = 'http://localhost:3008/live-agents/registerClient'
+    const payload = {
+      action: "survey_record",
+      token: getUrlQueryParams().token,
+      surveyDatetime: new Date().toISOString(),
+      datavalues: [
+        {
+          datapoint: "puKECcdj3M8",
+          value: checkboxConsultaGeneral,
+        },
+        {
+          datapoint: "nKQXSnhaVTD",
+          value: checkboxCitologia,
+        },
+        {
+          datapoint: "S7AXpql3zR5",
+          value: checkboxConsejeria,
+        },
+        {
+          datapoint: "QapYvc8bxqI",
+          value: checkboxEntrega,
+        },
+        {
+          datapoint: "TiXiw99bTxd",
+          value: checkboxAtencion,
+        },
+        {
+          datapoint: "BbIpwDoh596",
+          value: checkboxOtros,
+        },
+        {
+          datapoint: "MRsvIpVjrf2",
+          value: wasInformationDeliveredRadioButton,
+        },
+        {
+          datapoint: "Q0uOArWroLA",
+          value: question1Answer,
+        },
+        {
+          datapoint: "fHgpGfEIGuL",
+          value: question2Answer,
+        },
+        {
+          datapoint: "Yy3LLFvjb1V",
+          value: question3Answer,
+        },
+        {
+          datapoint: "TeRy5oTEmFo",
+          value: question4Answer,
+        },
+        {
+          datapoint: "IJqSoSxHz7X",
+          value: question5Answer,
+        },
+        {
+          datapoint: "M2YvHnEzuDa",
+          value: question6Answer,
+        },
+        {
+          datapoint: "oposne24pSc",
+          value: question7Answer,
+        },
+      ],
+    };
+
+    $.ajax({
+      type: "POST",
+      url: url,
+      data: payload,
+      success: endSurvey,
+      error: function(jqXHR, textStatus, errorThrown){
+        alert('Hubo un problema al enviar la encuesta.');
+      }
+    })
+  }
+
+  function endSurvey() {
+    if (currentStep < 4) {
+      $(".step" + currentStep).removeClass("active");
+      $(".section")
+        .eq(currentStep - 1)
+        .hide();
+      $(".wrapper").animate({ scrollTop: 0 }, "slow");
+      currentStep++;
+      $(".step" + currentStep).addClass("active");
+      $(".section")
+        .eq(currentStep - 1)
+        .show();
+      if (currentStep > 1) {
+        $("#back").show();
+      }
+    }
+    updateNextButtonState();
+    }
+
+
 
   // Assign the click event to icons
   $(".opt").click(handleIconClick);
   $(".opt").click(enableNextQuestion);
   $(".opt").click(enableSectionsWithSelectedOptions);
 
+  $btnSend.click(sendSurveyData);
+
   // Initially hide all 'answers selected'
   $(".answer-selected").hide();
 
-  $btnSend.click(function () {
+  /* $btnSend.click(function () {
     if (currentStep < 4) {
       $(".step" + currentStep).removeClass("active");
       $(".section")
@@ -195,7 +376,7 @@ $(document).ready(function () {
     }
     updateNextButtonState();
   });
-
+ */
   $btnNext.click(function () {
     if (currentStep < 4) {
       $(".step" + currentStep).removeClass("active");
